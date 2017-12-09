339ff87ccd4bd5c0b4542b0906b8398218aa69fe
ygorkagrishin <webygorkagrishin@gmail.com>
2017-12-09 11:46:00 +0300

    creat new files touch-pad and touch-phone

--- gulpfile.js
+++ gulpfile.js
@@ -66,7 +66,7 @@ gulp.task( 'watch', function () {
 
     gulp.watch( 'src/pug/**/**/*.pug', gulp.series( 'pug' ) );
     gulp.watch( 'src/static/fonts/**/*', gulp.series( 'fonts' ) );
-    gulp.watch( 'src/static/stylus/**/**/*.styl', gulp.series( 'stylus' ) );
+    gulp.watch( 'src/static/stylus/**/**/**/*.styl', gulp.series( 'stylus' ) );
     gulp.watch( 'src/static/scripts/**/*.js', gulp.series( 'scripts' ) );
 
 });
