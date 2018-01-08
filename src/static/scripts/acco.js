6044a3839d16c83a8955f52469591ca8709c4d85
ygorkagrishin <webygorkagrishin@gmail.com>
2018-01-08 17:17:00 +0300

    Устранены баги, удалены лишние классы

--- src/static/scripts/acco.js
+++ src/static/scripts/acco.js
@@ -1,5 +1,4 @@
 var Acco = function ( param ) {
-
 'use strict';
 
 var self = this;
@@ -10,36 +9,38 @@ priv.param = param;
 
 priv.acco = document.getElementById( priv.param.acco );
 
+priv.cls = {
+  section : priv.param.section !== undefined ? priv.param.section : 'js-acco__section',
+  trigger : priv.param.trigger !== undefined ? priv.param.trigger : 'js-acco__trigger'
+}
+
 priv.comp = {
-    section : priv.param.section,
-    trigger : priv.param.trigger,
-    container : priv.param.container
+  section : document.getElementsByClassName( priv.cls.section ),
+  trigger : document.getElementsByClassName( priv.cls.trigger )
 }
 
 priv.close = function ( array, cls ) {
-    for ( var i = 0; i <= array.length - 1; i++ )
-      if ( array[i].classList.contains( cls ) )
-        array[i].classList.remove( cls );
+  for ( var i = 0; i <= array.length - 1; i++ )
+  if ( array[i].classList.contains( cls ) )
+  array[i].classList.remove( cls );
 }
 
 priv.handler = function ( e ) {
-
   var target = e.target, currentSection;
 
   if ( target.tagName.toLowerCase() === 'a' ) e.preventDefault();
-
-  if ( target.classList.contains( priv.comp.trigger ) ) {
   
+  if ( target.classList.contains( priv.cls.trigger ) ) {
   currentSection = target;
-
-  while( !currentSection.classList.contains( priv.comp.section ) ) 
+  
+  while( !currentSection.classList.contains( priv.cls.section ) ) 
     currentSection = currentSection.parentNode;   
   
   if ( !currentSection.classList.contains( 'active') ) {
-    priv.close( priv.acco.getElementsByClassName( priv.comp.section ), 'active' ); 
+    priv.close( priv.comp.section, 'active' ); 
     currentSection.classList.add( 'active' ); }
   else { 
-    priv.close( priv.acco.getElementsByClassName( priv.comp.section ), 'active' ); }
+    priv.close( priv.comp.section, 'active' ); }
   }
 }
 
@@ -48,19 +49,9 @@ priv.acco.addEventListener( 'click', priv.handler );
 }
 
 var accoHorizontal = new Acco({
-
-    acco : 'acco-h',
-    section : 'js-acco-h__section',
-    trigger : 'js-acco-h__trigger',
-    container : 'js-acco-h__container'
-
+  acco : 'acco-horizontal'
 });
 
 var acco = new Acco({
-
-    acco : 'acco',
-    section : 'js-acco__section',
-    trigger : 'js-acco__trigger',
-    container : 'js-acco__container'
-    
+  acco : 'acco'    
 });
